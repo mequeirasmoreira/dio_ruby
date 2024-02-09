9c6d5e46f1103f44a0f963a0501b55667869abe5
mequeiras <126635118+mequeirasmoreira@users.noreply.github.com>
2024-02-09 19:34:36 -0300

    Add files via upload

---
+++ desafio.rb
@@ -0,0 +1,11 @@
+#Perguntar o nome :then guardar o nome
+print "Olá, qual o seu nome?"
+firstName = gets.chomp
+#Perguntar sobrenome :then guardar o sobrenome
+print "E qual o seu sobrenome?"
+middleName = gets.chomp
+#Perguntar idade :then guardar a idade
+print "Quantos anos você tem"
+age = gets.chomp
+#Imprimir: "#{firstName} #{middleName} #{age}"
+puts "#{firstName} #{middleName} #{age}"
